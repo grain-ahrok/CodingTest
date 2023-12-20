6a6cfd0cb0f396bf50076535701073e7b5015daf
arock1998 <arock1998@naver.com>
2023-12-20 21:42:28 +0900

    feat: 백준 2309 일곱난쟁이

--- 02_10주완성cpp/week_1/02_2309_일곱난쟁이.cpp
+++ 02_10주완성cpp/week_1/02_2309_일곱난쟁이.cpp
@@ -2,30 +2,86 @@
 using namespace std;
 
 int n = 9, k = 7;
-vector<int> height;
+vector<int> h;
+//  // 재귀함수로 풀기
 
 
-// TODO : make permutation 으로 풀기, // nCr = nCn-r 을 이요해서 풀기, // 재귀함수로 풀기
-void combine(int start, vector<int> v) {
-    if (v.size() == k && accumulate(v.begin(), v.end(), 0) == 100 ) {
-        for(int a : v) cout << a << "\n";
-        exit(0);
-    }
-    for(int i = start + 1; i < n; i++) {
-        v.push_back(height[i]);
-        combine(i, v);
-        v.pop_back();
-    }
-}
-
-
-int main() {
+// case1 : make permutation으로 풀기
+void case1() {
+    vector<int> height;
     int temp;
-    for(int i = 0; i < 9; i++) {
+    for(int i = 0; i < n; i++) {
         cin >> temp;
         height.push_back(temp);
     }
     sort(height.begin(), height.end());
-    vector<int> empty;
-    combine(-1, empty);
+    do {
+        if(accumulate(height.begin(), height.begin()+7, 0) == 100) {
+            for(int i = 0; i < 7; i++) cout << height[i] << " ";
+            return;
+        }
+    } while(next_permutation(height.begin(), height.end()));
+}
+
+// nCr = nCn-r 을 이용해서 풀기,
+void case2() {
+    vector<int> height;
+    int temp;
+    for(int i = 0; i < n; i++) {
+        cin >> temp;
+        height.push_back(temp);
+    }
+    sort(height.begin(), height.end());
+    int sum = accumulate(height.begin(), height.end(), 0);
+
+    pair<int, int> ret;
+    for(int i = 0; i < n; i++) {
+        for(int j = i+1; j < n; j++ ) {
+            if(height[i]+height[j] == sum-100) {
+                ret = {i, j};
+            } 
+        }
+    }
+
+    for(int i = 0; i < n; i ++) {
+        if(ret.first == i || ret.second == i ) continue;
+        cout << height[i] << " ";
+    }
+}
+
+// 재귀함수로 풀기
+void combine(int start, vector<int> v) {
+    if(v.size() == k && accumulate(v.begin(), v.end(), 0) == 100) {
+        for(int a : v) cout << a << " ";
+        exit(0);
+        return;
+    }
+
+    for(int i = start+1; i < n; i++) {
+        v.push_back(h[i]);
+        combine(i, v);
+        v.pop_back();
+    }
+    return;
+}
+
+void case3() {
+    int temp;
+    for(int i = 0; i < n; i++) {
+        cin >> temp;
+        h.push_back(temp);
+    }
+    sort(h.begin(), h.end());
+
+    vector<int> v;
+    combine(-1, v);
+    return;
+}
+
+
+
+int main() {
+    case3();
+
+    return 0;
 }
