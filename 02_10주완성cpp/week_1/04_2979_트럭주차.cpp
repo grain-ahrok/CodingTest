b86ff6e2938db4321a997f640099c098cb4344cd
arock1998 <arock1998@naver.com>
2024-01-07 23:53:47 +0900

    feat: 백준 2979 쌤풀 추가

--- 02_10주완성cpp/week_1/04_2979_트럭주차.cpp
+++ 02_10주완성cpp/week_1/04_2979_트럭주차.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-
+void my_sol() {
     int a, b, c;
     cin >> a >> b >> c;
 
@@ -23,6 +22,27 @@ int main() {
         else if(i == 3) total += 3*c;
 
     }
-
     cout << total;
+}
+
+void t_sol() {
+    int A, B, C, a, b, cnt[104], ret;
+    cin >> A >> B >> C;
+    for(int i = 0; i < 3; i++) {
+        cin >> a >> b;
+        for(int j = a; j <=b; j++) cnt[j]++;
+    }
+    for(int j = 1; j < 100; j++) {
+        if(cnt[j]) {
+            if(cnt[j] = 1) ret += A;
+            else if (cnt[j] == 2) ret += B * 2;
+            else if (cnt[j] == 3) ret += C * 3;
+        }
+    }
+    cout << ret << "\n";
+    return 0;
+}
+
+int main() {
+
 }
