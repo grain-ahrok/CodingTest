d2555b419c5bd5c921654f80f529cd035525fac1
arock1998 <arock1998@naver.com>
2024-01-08 18:57:14 +0900

    feat: 인접 행렬 예제

---
+++ 02_10주완성cpp/week_0/00_test.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> v;
+
+void make_permutation(int n, int r, int depth) {
+    if(depth == r) {
+        for(int a : v) cout << a << " ";
+        cout << endl;
+        return;
+    }
+
+    for(int i = depth; i < n; i++) {
+        swap(v[i], v[depth]);
+        make_permutation(n, r, depth + 1);
+        swap(v[i], v[depth]);
+    }
+}
+
+int main() {
+    for(int i = 1; i < 4; i++) v.push_back(i);
+    make_permutation(3, 3, 0);
+}
+
+
+
+
+
+void make_permutation(int n, int r, int depth) {
+    if(depth == r) {
+        for(int i : v) cout << i << " ";
+        cout << endl;
+    }
+
+    for(int i = depth; i < n; i ++) {
+        swap(v[depth], v[i]);
+        make_permutation(n, r, depth + 1);
+        swap(v[depth], v[i]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
