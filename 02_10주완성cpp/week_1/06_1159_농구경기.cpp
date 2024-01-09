8122bdf55514a62c8c2e35b4019f2411f8af6e66
arock1998 <arock1998@naver.com>
2024-01-09 22:46:03 +0900

    feat: 1159 풀이 추가

--- 02_10주완성cpp/week_1/06_1159_농구경기.cpp
+++ 02_10주완성cpp/week_1/06_1159_농구경기.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+void my_sol() {
     int alpa[26] = {0,};
-
     int cnt;
     cin >> cnt;
 
     bool canCompet = false;
-
     string name;
     for(int i = 0; i < cnt; i++) {
         cin >> name;
@@ -24,4 +22,23 @@ int main() {
     if(canCompet == false) {
         cout << "PREDAJA";
     }
+}
+
+void t_sol(){
+    int n, cnt[26];
+    string s, ret;
+
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> s;
+        cnt[s[0] - 'a']++;
+    }
+    for(int i = 0; i < 26; i++) if(cnt[i] >= 5) ret += (i+'a');
+    if(ret.size()) cout << ret;
+    else cout << "PREDAJA";
+
+}
+
+int main() {
+
 }
