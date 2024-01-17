eacf3f524014d8d903b96d1f369e8e103c4c0c96
arock1998 <arock1998@naver.com>
2024-01-17 22:35:54 +0900

    feat: 트리 순회

---
+++ 02_10주완성cpp/week_2/12_tree_traversal.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void postOrdre(int here) [
+    if(visited[here] == 0){
+        if(adj[here].size() == 1) postOrder(adj[here][0]);
+        if(adj[here].size() == 2) {
+            postOrder(adj[here][0]);
+            postOrder(adj[here][1]);
+        }
+        visited[here] = 1;
+        cout << here << " ";
+    }
+]
+
+
+
+void preOrder(int here) {
+    if( visited[here] == 0 ) {
+        visited[here] = 1;
+        cout << here << ' ';
+        if(adj[here].size() == 1) preOrder(adj[here][0]);
+        if(adj[here].size() == 1) {
+            preOrder(adj[here][0]);
+            preOrder(adj[here][1]);
+        }
+    }
+}
+
+
+void inOrder(int here) {
+    if(visited[here] == 0) {
+        if(adj[here].size() == 1) {
+            inOrder(adj[here][0]);
+            visited[here] = 1;
+            cout << here << " ";
+        } else if (adj[here].size() == 2) {
+            inOrder(adj[here][0]);
+            visited[here] = 1;
+            cout << here << " ";
+        } else {
+            visited[here] = 1;
+            cout << here << " ";
+        }
+    }
+}
+
+int main() {
+    adj[1].push_back(2);
+    adj[1].push_back(3);
+    adj[2].push_back(4);
+    adj[2].push_back(5);
+    int root = 1;
+    cout << "트리순회 : postOrder \n";
+    postOrder(root); memset(visited, 0, sizeof(visited));
+    cout << "트리순회 : preOrder \n";
+    preOrder(root); memset(visited, 0, sizeof(visited));
+    cout << "트리 순회 : inOrder \n";
+    inOrder(root);
+    return 0;
+}
