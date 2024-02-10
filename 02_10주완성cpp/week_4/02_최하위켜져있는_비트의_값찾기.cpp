59584bb4ac2b5673419c74625e2ad7bbf9729356
arock1998 <arock1998@naver.com>
2024-02-10 23:26:04 +0900

    feat: 비트마스킹 공부

---
+++ 02_10주완성cpp/week_4/02_최하위켜져있는_비트의_값찾기.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int S = 18;
+    int value = S & -S;
+    cout << value;
+}
---
+++ 02_10주완성cpp/week_4/03_n비트_모두켜기.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 6;
+    cout << (1 << n) -1;
+}
