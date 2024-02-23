92e0f849ce0345e2a40d60fb9291605a530a19d9
arock1998 <arock1998@naver.com>
2024-02-23 23:27:31 +0900

    feat: 백준 12869

---
+++ 02_10주완성cpp/week_3/08_12869_뮤탈리스크.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> damage = {9, 3, 1};
+vector<int> ret;
+int N;
+
+
+void combine(vector<int> _scv, vector<int> _damage, int cnt) {
+    if(max_element(_ret.begin(), _ret.end()) <= 0) {
+        cout << cnt;
+        return;
+    }
+
+    do {
+        for(int i = 0; i < N; i++) {
+            _scv[i] -= 
+        }
+
+    } while (next_permutation(_damage.begin(), _damage.end()));
+
+}
+
+int main() {
+    cin >> N;
+    vector<int> scv;
+
+    int temp;
+    for(int i = 0; i < N; i++) {
+        cin >> temp;
+        scv.push_back(temp);
+    }
+
+
+    do {
+        combine(scv, damage, 0);
+    } while(next_permutation(damage.begin(), damage.end()));
+        
+
+
+
+
+}
