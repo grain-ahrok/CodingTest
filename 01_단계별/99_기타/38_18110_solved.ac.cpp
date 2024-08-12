6d619386adb56de3fbfd18db42cbff0a60ac9ab9
arock1998 <arock1998@naver.com>
2024-08-13 01:08:57 +0900

    feat: 백준 18110 solved.ac

--- 01_단계별/99_기타/38_18110_solved.ac.cpp
+++ 01_단계별/99_기타/38_18110_solved.ac.cpp
@@ -2,23 +2,34 @@
 using namespace std; 
 
 int main () {
-    int N;
-    int val[300001];
-    int tmp = 0;
+    int N, tmp;
+    vector<int> v;
+    int sum = 0;
 
     cin >> N;
-    for(int i = 0; i < N; i++) {
-        cin >> val[i];
-        tmp += val[i];
-        val[i] = tmp;
+    if(N == 0) {
+        cout << "0";
+        return 0;
     }
 
-    // for(int i = 0; i < N; i++) {
-    //     cout << val[i] << " ";
-    // }
+    for(int i = 1; i <= N; i++) {
+        cin >> tmp;
+        v.push_back(tmp);
+        sum += tmp;
+    }
 
-    int rmN = round(N*0.15);
-    int res = round((double)(val[N-rmN-1] - val[rmN-1]) / (N-rmN*2));
-    cout << val[N-rmN-1] - val[rmN-1] << " ";
-    cout << res;
+    sort(v.begin(), v.end());
+
+    int rmN = (int)(round(N*0.15));
+    int total = 0; 
+
+    for(int i = 0; i < rmN; i++) {
+        total += v[i];
+        total += v[N-i-1];
+    }
+
+    sum -= total;
+    cout << round((double)sum / (N-2*rmN));
+
+
 }
