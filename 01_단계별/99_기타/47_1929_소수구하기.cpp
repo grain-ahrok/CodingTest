81fee00c8f052d9a2c3d69578be8b3856fb47c4d
arock1998 <arock1998@naver.com>
2024-08-27 00:28:03 +0900

    feat: 백준 1929 소수구하기

--- 01_단계별/99_기타/47_1929_소수구하기.cpp
+++ 01_단계별/99_기타/47_1929_소수구하기.cpp
@@ -1,1 +1,28 @@
-\
+#include <bits/stdc++.h>
+using namespace std;
+
+void check() {
+
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    int res[1000001] = {};
+    fill(res, res + M+1, 1);
+    res[1] = 0;
+
+    for(int i = 2; i <= M; i++) {
+        if(res[i] == 0) continue;
+        for(int j = 2; i * j<= M; j++) {
+            res[i*j] = 0;
+        }
+    }
+
+    for(int i = N; i <= M; i++) {
+        if(res[i]) cout << i << "\n";
+    }
+
+
+}
